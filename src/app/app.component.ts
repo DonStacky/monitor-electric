c4ba412cb7538a23f8abd37e92356220d51a3d41
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2024-01-11 21:17:22 +0300

    feat: add mock data and handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TableRowComponent } from './table-row/table-row.component';
+import { ROWS } from './mock-rows';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TableRowComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'monitor-electric';
+  rows = ROWS;
+
+  addRow() {
+    console.log("add row");
+  }
+
+  editRow() {
+    console.log("edit row");
+  }
+
+  deleteRow() {
+    console.log("delete row");
+  }
 }
---
+++ src/app/row.ts
@@ -0,0 +1,12 @@
+export interface Row {
+  date: string;
+  time: string;
+  source: string;
+  phase: string;
+  voltage: string;
+  amperage: string;
+  power: string;
+  repower: string;
+  angle: string;
+  name: string;
+}
